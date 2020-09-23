8e8cbc722917d5228d032d922a2bed32f76b647b
Alejandro Ahogado <a.ahogado@uniandes.edu.co>
2020-09-22 22:24:01 -0500

    test actualización

---
+++ test/testModeloEstructuraDatos/testTablaHashLinearProbing.java
@@ -0,0 +1,69 @@
+package testModeloEstructuraDatos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testTablaHashLinearProbing {
+
+	@Test
+	void testTablaHashLinearProbing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPut() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testKeySet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testValueSet() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDarPrimerElemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDarUltimoElemento() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConocerPeliculasAnoProduccionLinearProbing() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/testModeloLogico/testModelo.java
@@ -0,0 +1,34 @@
+package testModeloLogico;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testModelo {
+
+	@Test
+	void testModelo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCargarDatosTablaHashLinearProbing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCargarDatosTablaHashSeparateChaining() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConocerPeliculasAnoProduccionLinearProbing() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testConocerPeliculasAnoProduccionSeparateChaining() {
+		fail("Not yet implemented");
+	}
+
+}
