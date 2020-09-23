fe6f6d5a5f6309efbb8b8bb7d91773d68224f37c
Alejandro <a.alcaraz@uniandes.edu.co>
2020-09-22 22:02:09 -0500

    mensaje leve

--- src/modeloLogico/Modelo.java
+++ src/modeloLogico/Modelo.java
@@ -183,7 +183,9 @@ public class Modelo {
 	}
 
 	public String conocerPeliculasAnoProduccionLinearProbing(int production_year, String company_name) {
+		System.out.println("Peliculas producidas por la compania "+ company_name + " en el  año :  "+ production_year+ "\n");
 		return tablaLinearProbing.conocerPeliculasAnoProduccionLinearProbing(company_name+production_year);
+		
 	}
 
 	public void conocerPeliculasAnoProduccionSeparateChaining(int production_year, String company_name) 
